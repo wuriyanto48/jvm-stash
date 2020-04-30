c824cb6c4b11eae44c0f1845cb41037b5a67d5f2
Wuriyanto <wuriyanto48@yahoo.co.id>
2020-05-01 05:46:00 +0700

    initial commit

---
+++ src/main/java/com/wuriyanto/jvmstash/App.java
@@ -0,0 +1,13 @@
+package com.wuriyanto.jvmstash;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
